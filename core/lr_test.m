04a4bfc7b9f4b8d0cfeb40875e5f2c65909050c1
chenhao1UMBC <chenhao1@umbc.edu>
2018-03-13 07:14:06 -0700

    get the right logistic regression test file and it can repeat the result for the LM report

--- core/lr_test.m
+++ core/lr_test.m
@@ -1,16 +1,45 @@
-function [acc, errortable, acc_av] = lr_test(Dict, Database, Z, B)
+function [acc, acc_av] = lr_test(Dict, Database, Z, B)
 
 C = 6; % 6 classes
-ln_test=length(Database.test_mixlabel);
-true_labels = Database.test_mixlabel;
+featln = 4;
+n = Database.N_c;
+N = size(Database.test_mixlabel, 2);
+ln_test= N/featln;
 W = Dict.W;
-wz = (W'*Z);
+wz = (W'*aoos(Z, featln, N));
 pre_prob = mnrval(B, wz');
 [~,labels_pre] = sort(pre_prob, 2, 'descend');
 labels_pre = labels_pre';
 
-% run mix_n_ld34
-run mix_ntable
+acc_t = zeros(C, n);
+% run mix_ntable
+for section = 1:n    
+    sect = labels_pre(:,1+(section-1)*ln_test/n:section*ln_test/n);
+    % we only care about lower power accurcy
+    [acc_t(:,section)] = mix_table_power(ln_test/n, section, sect);
+end
+
+acc = sum(acc_t, 2)/50/size(combnk(1:5,n-1),1);
+acc_av = sum(acc)/C;
+
+% this fucntion is shared data funcion to calc each portion of power
+function [acc0] = mix_table_power(ln_test_part, whichpart,labels_pre_part)        
+    Ncombs=max(Database.cv_mixlabel);
+    c = combnk(1:6,n); % ble bt fhss1 zb
+    acc0=zeros(C,1); % only score for true-positive and true negative
+
+    for indCl=1:Ncombs
+        temp = c(indCl,:);
+        indClnm(1) = temp(whichpart);
+        indClnm(2:6) = Theother3(indClnm(1));
+        for ii=1:ln_test_part/Ncombs          
+                acc0(indClnm(1))=acc0(indClnm(1))+length(find...
+                    (labels_pre_part(1:n,ii+(indCl-1)*ln_test_part/Ncombs)==indClnm(1)));            
+        end
+
+    end   
+   
+end
 
 
 end % end of the function file
--- testing.m
+++ testing.m
@@ -5,8 +5,8 @@ addpath(genpath('./fddlow'))
 addpath(genpath('./data'))
 
 % load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat')
-% load('Delta_FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta0.1.mat')
-load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta10.mat')
+load('Delta_FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta0.1.mat')
+% load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta10.mat')
 mixture_n = 2; % mixture_n classes mixture
 [Database]=load_data(mixture_n);% the equal power mixture, 400 samples per combination
 if exist('Dict')==1
@@ -16,4 +16,4 @@ Z = sparsecoding_mix_test(Dict_mix, Database, opts);
 
 load('B_X_Y.mat')
 % load('SNR2000B_X_Y_mix.mat')
-[acc,errortable, acc_avtest] = lr_test(Dict_mix, Database, Z, B);
+[acc, acc_av] = lr_test(Dict_mix, Database, Z, B);

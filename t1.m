d8ae392c835c1bb9dcc5f3344af4c28de65238ff
chenhao1UMBC <chenhao1@umbc.edu>
2018-03-20 07:57:14 -0700

    change data rout

--- t1.m
+++ t1.m
@@ -3,21 +3,27 @@ clear
 clc;
 tic
 
-addpath(genpath('./fddlow'))
-addpath(genpath('./data'))
+addpath(genpath('.././fddlow'))
+addpath(genpath('.././data'))
 % load data
 [Database]=load_data;
 
 %% training dictionary
-% for table 1 algorithm
+
+for ii = [0.3, 0.5, 0.7, 1.3, 1.5, 1.7, 2, 2.25, 2.5, 2.7, 3]
 K=100;
 lbmd=1.5;
 mu=0.1;
 nu=1e3;
 Q=16;% this is wq without negative
-beta = 5;
-[opts]=loadoptions(K,lbmd,mu,Q,nu,beta);
+beta = ii;
+SNR = 2000;
+[opts]=loadoptions(K,lbmd,mu,Q,nu,beta, SNR);
 
 % for table 2 algorithm
 Dict_mix = FDDLOW_table2(Database.tr_data,Database.tr_label,opts);
-save(opts.mixnm,'Dict_mix','opts')
+save(opts.mixnm,'FDDLOW_mix','opts')
+
+% this part will give the logistic regression result
+trainLR_dif(opts, Database);
+end

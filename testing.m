7db1b20b0db6a4ec6e79b4d175bf293bce380d41
chenhao1 <chenhao1@umbc.edu>
2018-03-22 07:42:23 -0400

    change powercases index and run roughly the result

--- testing.m
+++ testing.m
@@ -4,22 +4,24 @@ clc
 addpath(genpath('./fddlow'))
 addpath(genpath('./data'))
 
-% load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat')%FDDLO
+load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat')%FDDLO
 % load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta0.1.mat')
 % load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta1.mat')
-load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta5.mat')
+% load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta5.mat')
 % load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta10.mat')
 mixture_n = 2; % mixture_n classes mixture
-[Database]=load_data(mixture_n);% the equal power mixture, 400 samples per combination
+SNR = 2000;
+powercases = 2;
+[Database]=load_data(mixture_n, SNR, powercases);% the equal power mixture, 400 samples per combination
 if exist('Dict')==1
     Dict_mix = Dict;
 end 
 Z = sparsecoding_mix_test(Dict_mix, Database, opts);
 
 sparsity = mean(sum(Z ~= 0))
-% load('B_X_Y.mat')%FDDLO
+load('B_X_Y.mat')%FDDLO
 % load('SNR2000_beta0.1B_X_Y.mat')
 % load('SNR2000_beta1B_X_Y.mat')
-load('SNR2000_beta5B_X_Y.mat')
+% load('SNR2000_beta5B_X_Y.mat')
 % load('SNR2000_beta10B_X_Y.mat')
 [acc, acc_av] = lr_test(Dict_mix, Database, Z, B)

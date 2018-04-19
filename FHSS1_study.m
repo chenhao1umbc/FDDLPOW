49fc13b622eb27483f906dd7a07fc685ac63739a
chenhao1 <chenhao1@umbc.edu>
2018-04-19 16:26:15 -0400

    minor change for plots

--- FHSS1_study.m
+++ FHSS1_study.m
@@ -1,9 +1,12 @@
 
-% show projection
+% all the codes show projection for L =2
+symb = {'x','sq', '*', '>','^','.'};
 for ii = 1:6
 rt(ii, :) = H(:,ii)'*W'*aoos(Z,4,size(Z, 2))/norm(H(:, ii),2);
-figure; plot(rt(ii, :),'x'); grid on;grid minor
+% figure; 
+plot(rt(ii, 1:end/2), symb{ii}); grid on; hold on
 end
+legend('ble', 'bt', 'fhss1', 'fhss2', 'wif1', 'wifi2')
 
 pr = rt(:, [251:300,1001:1050]);
 [~, lb] = sort(pr, 1, 'descend');
@@ -20,14 +23,14 @@ Z2 = tr_Zs(:, 301:600);
 Z3 = tr_Zs(:, 601:900);
 Z6 = tr_Zs(:, 1501:1800);
 figure; imagesc(Z2+ Z3); title 'trained BT+FHSS1';colorbar
-figure; imagesc(Z2+ Z6); title 'trained BT+Zigbee';colorbar
+figure; imagesc(Z2+ Z6); title 'trained BT+Wifi2';colorbar
 
 Z_weak2_3 = Zs(:, 251:300); % equal power case Z_weak2_3 == Z_2_weak3
 Z_2_weak3 = Zs(:, 1001:1050);
 
 figure; imagesc(Z2); title 'bt'; colorbar
 figure; imagesc(Z3); title 'FHSS1'; colorbar
-figure; imagesc(Z6); title 'zigbee'; colorbar
+figure; imagesc(Z6); title 'wifi2'; colorbar
 figure; imagesc(Z_weak2_3); title 'test bt + FHSS1'; colorbar
 % figure; imagesc(Z_weak2_3); title 'weak bt + FHSS1'; colorbar
 % figure; imagesc(Z_2_weak3); title 'bt + weak FHSS1'; colorbar
@@ -35,12 +38,12 @@ close all
 
 figure; imagesc(W'*Z2); title 'bt'; colorbar
 figure; imagesc(W'*Z3); title 'FHSS1'; colorbar
-figure; imagesc(W'*Z6); title 'zigbee'; colorbar
+figure; imagesc(W'*Z6); title 'wifi2'; colorbar
 figure; imagesc(W'*Z_weak2_3); title 'test bt + FHSS1'; colorbar
 % figure; imagesc(W'*Z_weak2_3); title 'weak bt + FHSS1'; colorbar
 % figure; imagesc(W'*Z_2_weak3); title 'bt + weak FHSS1'; colorbar
 figure; imagesc(W'*(Z2 + Z3)); title 'bt + FHSS1'; colorbar
-figure; imagesc(W'*(Z2 + Z6)); title 'bt + zigbee'; colorbar
+figure; imagesc(W'*(Z2 + Z6)); title 'bt + wifi2'; colorbar
 close all
 
 Z0 = (3.16*Z2+Z3);

b96e43aea1a1ecb5eaa608402baa69de8d796bd7
chenhao1UMBC <chenhao1@umbc.edu>
2018-04-19 08:41:24 -0700

    look at the result of equal power case

---
+++ FHSS1_study.m
@@ -0,0 +1,52 @@
+
+% show projection
+for ii = 1:6
+rt(ii, :) = H(:,ii)'*W'*aoos(Z,4,size(Z, 2))/norm(H(:, ii),2);
+figure; plot(rt(ii, :),'x'); grid on;grid minor
+end
+
+pr = rt(:, [251:300,1001:1050]);
+[~, lb] = sort(pr, 1, 'descend');
+imagesc(pr)
+figure
+imagesc(Dict_mix.D); title 'Dictionary'; colorbar
+
+
+Zs = aoos(Z,4,size(Z, 2));
+tr_Zs = aoos(Dict_mix.Z,4,size(Dict_mix.Z, 2));
+figure; imagesc([W'*Zs(:, [251:300,1001:1050]), W'*tr_Zs(:, [301:900, 1501:1800])])
+
+Z2 = tr_Zs(:, 301:600);
+Z3 = tr_Zs(:, 601:900);
+Z6 = tr_Zs(:, 1501:1800);
+figure; imagesc(Z2+ Z3); title 'trained BT+FHSS1';colorbar
+figure; imagesc(Z2+ Z6); title 'trained BT+Zigbee';colorbar
+
+Z_weak2_3 = Zs(:, 251:300); % equal power case Z_weak2_3 == Z_2_weak3
+Z_2_weak3 = Zs(:, 1001:1050);
+
+figure; imagesc(Z2); title 'bt'; colorbar
+figure; imagesc(Z3); title 'FHSS1'; colorbar
+figure; imagesc(Z6); title 'zigbee'; colorbar
+figure; imagesc(Z_weak2_3); title 'test bt + FHSS1'; colorbar
+% figure; imagesc(Z_weak2_3); title 'weak bt + FHSS1'; colorbar
+% figure; imagesc(Z_2_weak3); title 'bt + weak FHSS1'; colorbar
+close all
+
+figure; imagesc(W'*Z2); title 'bt'; colorbar
+figure; imagesc(W'*Z3); title 'FHSS1'; colorbar
+figure; imagesc(W'*Z6); title 'zigbee'; colorbar
+figure; imagesc(W'*Z_weak2_3); title 'test bt + FHSS1'; colorbar
+% figure; imagesc(W'*Z_weak2_3); title 'weak bt + FHSS1'; colorbar
+% figure; imagesc(W'*Z_2_weak3); title 'bt + weak FHSS1'; colorbar
+figure; imagesc(W'*(Z2 + Z3)); title 'bt + FHSS1'; colorbar
+figure; imagesc(W'*(Z2 + Z6)); title 'bt + zigbee'; colorbar
+close all
+
+Z0 = (3.16*Z2+Z3);
+result0 = pinv(H)*W'*aoos(Z0,4,size(Z0, 2));
+[~, labels_pre0] = sort(result0, 1, 'descend');
+
+Z0 = (10* Z2+Z3);
+result0 = pinv(H)*W'*aoos(Z0,4,size(Z0, 2));
+[~, labels_pre0] = sort(result0, 1, 'descend');
--- testing_equalizer.m
+++ testing_equalizer.m
@@ -4,14 +4,14 @@ clc
 addpath(genpath('.././fddlow'))
 addpath(genpath('.././data'))
 
-% load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat') %FDDLO
+load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat') %FDDLO
 % load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta1.7.mat')
 % load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q32_nu1000_beta1.7.mat')
 
 
-mixture_n = 3; % mixture_n classes mixture
+mixture_n = 2; % mixture_n classes mixture
 SNR = 2000;
-pctrl.equal = 0; % 1 means eqaul power, 0 non-equal
+pctrl.equal = 1; % 1 means eqaul power, 0 non-equal
 pctrl.db = 10; % dynamic ratio is 3, 6, 10, 20, 40db
 % the equal power mixture, 400 samples per combination
 [Database]=load_data(mixture_n, SNR, pctrl);

e6fa1870f7711507007f75552e869a61afc5b075
chenhao1UMBC <chenhao1@umbc.edu>
2018-04-20 13:13:02 -0700

    commented logistic regression part

--- t1.m
+++ t1.m
@@ -11,21 +11,21 @@ addpath(genpath('.././data'))
 %% training dictionary
 for ii = [16, 32]
     for jj = 1:10
-K=100;
-lbmd=1.5;
-mu=0.1;
-nu=1e3;
-Q= ii;
-beta = 1.7;
-SNR = 2000;
-[opts]=loadoptions(K,lbmd,mu,Q,nu,beta, SNR);
-opts.rng = jj;
+        K=100;
+        lbmd=1.5;
+        mu=0.1;
+        nu=1e3;
+        Q= ii;
+        beta = 1.7;
+        SNR = 2000;
+        [opts]=loadoptions(K,lbmd,mu,Q,nu,beta, SNR);
+        opts.rng = jj;
 
-% for table 2 algorithm
-Dict_mix = FDDLOW_table2(Database.tr_data,Database.tr_label,opts);
-save(['rng', num2str(jj), opts.mixnm],'Dict_mix','opts')
+        % for table 2 algorithm
+        Dict_mix = FDDLOW_table2(Database.tr_data,Database.tr_label,opts);
+        save(['rng', num2str(jj), opts.mixnm],'Dict_mix','opts')
 
-% this part will give the logistic regression result
-trainLR_dif(opts, Database);
+        % % this part will give the logistic regression result
+        % trainLR_dif(opts, Database);
     end
 end

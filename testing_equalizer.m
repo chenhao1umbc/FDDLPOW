33c65f3cbcda530a2c35fe9656a7d2d71f5e8e9e
chenhao1UMBC <chenhao1@umbc.edu>
2018-04-20 12:35:29 -0700

    Changes a lot for FHSS1_study

--- testing_equalizer.m
+++ testing_equalizer.m
@@ -4,10 +4,10 @@ clc
 addpath(genpath('.././fddlow'))
 addpath(genpath('.././data'))
 
-load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat') %FDDLO
-% load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta1.7.mat')
+% load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat') %FDDLO
+% load(['rng',num2str(ii), 'FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta1.7.mat'])
+load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta1.7.mat')
 % load('FDDLOW_mix_k100_lmbd1.5_mu0.1_Q32_nu1000_beta1.7.mat')
-
 
 mixture_n = 2; % mixture_n classes mixture
 SNR = 2000;
@@ -15,6 +15,7 @@ pctrl.equal = 1; % 1 means eqaul power, 0 non-equal
 pctrl.db = 10; % dynamic ratio is 3, 6, 10, 20, 40db
 % the equal power mixture, 400 samples per combination
 [Database]=load_data(mixture_n, SNR, pctrl);
+
 if exist('Dict')==1
     Dict_mix = Dict;
 end
@@ -29,7 +30,6 @@ M = Dict_mix.Z*H3;
 H = W'*M;
 result = pinv(H)*W'*aoos(Z,4,size(Z, 2));
 [~, labels_pre] = sort(result, 1, 'descend');
-
 
 opts.C = C; % 6 classes
 featln = Database.featln;

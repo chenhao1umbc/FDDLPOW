04aa48481de3bdf51117123bf3b24916e6c421ee
chenhao1UMBC <chenhao1@umbc.edu>
2018-04-24 08:52:25 -0700

    v0.3 done with fddlow, moving to see how it works if we change the scattering to spectrogram

---
+++ plots/plot_spect_v2.m
@@ -0,0 +1,28 @@
+close all
+clear
+
+smp_len = 4e6;
+k = 3; % which sample
+
+dir='/home/chenhao/Matlab/LMData4/DataCollection/';
+sig_name = {'ble_all', 'bt_all', 'fhss1_all', 'fhss2_all', 'wifi1_all', 'wifi2_all'};
+var_name = 'x';
+whichsignal = 3;
+t_resol = 100;
+
+fname = [dir sig_name{whichsignal}];
+obj=matfile(fname);
+x=obj.(var_name)(((1+(k-1)*smp_len):k*smp_len),1);
+% x=circshift(x,1e6);
+
+len_x = length(x); % xlen
+len_window = floor(len_x/t_resol);
+%nov = floor(nsc/2); % hop
+%nff = max(256,2^nextpow2(nsc));% nfft number of fft points 
+
+spectrogram(x,len_window,[],[],4e7,'yaxis','centered'); % 4e7 is the sampling frequency
+sp_x = spectrogram(x,len_window,[],1024,4e7,'yaxis','centered');
+
+figure
+imagesc(abs(sp_x))
+title(sig_name{whichsignal})

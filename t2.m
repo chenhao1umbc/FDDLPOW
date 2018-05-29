210eda29ce53bb614adcc78c7f5d1a67f36a283f
chenhao1UMBC <chenhao1@umbc.edu>
2018-05-29 12:56:19 -0700

    change t1.m to load dictionary, not done yet

--- t2.m
+++ t2.m
@@ -1,41 +1,46 @@
-% temp file to tune beta with logistic regression
+% table 2
 
-clear 
-clc
+close all
+clear
+clc;
+tic
 
-addpath(genpath('./fddlow'))
-addpath(genpath('./data'))
+addpath(genpath('.././fddlow'))
+addpath(genpath('.././data'))
 
-t = [0, 0.1:0.2:0.7, 1, 1.3:0.2:1.7, 2:0.25:2.5, 2.7, 3, 5, 10];
-tt = [3,6,10,20,40];
-for ii = 1:length(t) % loop through beta
-    for jj = 1:length(tt)
-        if ii == 1
-            load('DDLMDmix4_k100_lmbd1.5_mu0.1_Q16_nu1000iter_100.mat')%FDDLO
-%             load('B_X_Y.mat')%FDDLO
-            load('B_X_Y_pure.mat')%FDDLO
-        else
-            load(['FDDLOW_mix_k100_lmbd1.5_mu0.1_Q16_nu1000_beta', num2str(t(ii)), '.mat'])
-%             load(['SNR2000_beta', num2str(t(ii)), 'B_X_Y.mat'])
-            load(['beta', num2str(t(ii)), 'B_X_Y_pure.mat'])
-        end
-        mixture_n = 2; % mixture_n classes mixture
-        SNR = 2000;
-        pctrl.equal = 0; % 1 means eqaul power, 0 non-equal
-        pctrl.db = tt(jj); % dynamic ratio is 3, 6, 10, 20, 40db
-        [Database]=load_data(mixture_n, SNR, pctrl);% the equal power mixture, 400 samples per combination
-        if exist('Dict')==1
-            Dict_mix = Dict;
-        end 
-        Z = sparsecoding_mix_test(Dict_mix, Database, opts);
-        sparsity = mean(sum(Z ~= 0))
-        [acc_weak, acc_weak_av, acc_all] = lr_test(Dict_mix, Database, Z, B, pctrl)
-        
-        record_weak_av(ii, jj) = acc_weak_av; 
-        record_all(ii, jj) = acc_all;  
-    end
+% load data
+mixture_n = 2; % mixture_n classes mixture
+SNR = 2000;
+pctrl.equal = 0; % 1 means eqaul power, 0 non-equal
+pctrl.db = 3; % dynamic ratio is 3, 6, 10, 20, 40db
+
+% the equal power mixture, 400 samples per combination
+[Database]=load_data_new(mixture_n, SNR, pctrl);
+% [Database]=load_data;
+% Database = load_data_spectr(1);
+
+%% training dictionary
+% load settings
+K = 100;
+lbmd = 0.00001;
+mu=0.1;
+nu= 1e3;
+Q=16;% this is wq without negative
+beta = 1;
+SNR = 2000;
+
+for K = [100, 150, 200, 250]
+for lbmd = [0.001 0.005 0.01 1e-4]
+for mu=[1, 0.1, 0.01, 0.001 0.0001]
+[opts]=loadoptions(K,lbmd,mu,Q,nu,beta, SNR);
+
+% for table 2 algorithm
+Dict = FDDLOW_table2(Database.tr_data,Database.tr_label,opts);
+if Dict.iter > 80
+    save(opts.Dictnm,'Dict','opts')
 end
 
-record_all(:,6) =t(1:end)';
-record_weak_av(:,6) =t(1:end)';
-save('record_lr', 'record_all', 'record_weak_av')
+end
+end
+end
+

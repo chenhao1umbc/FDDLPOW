ed9a22eda0eeaeabc8b013cd8630e5e39438b72e
chenhao1UMBC <chenhao1@umbc.edu>
2018-12-24 07:37:36 -0800

    adding some log of results

--- t1_esc.m
+++ t1_esc.m
@@ -10,24 +10,41 @@ addpath(genpath('.././data'))
 
 %% load settings
 % do traing or do crossvalidation
-do_training = 0;
-cvortest = [1, 0]; % [docv, dotest] cannot be [1, 1]
+do_training = 1;
+cvortest = [0, 1]; % [docv, dotest] cannot be [1, 1]
 
 mixture_n = 1; % mixture_n classes mixture, = 1,2,3 (1 means non -mixture)
 pctrl.db = 0; % dynamic ratio is 0 3, 6, 10, 20 db
 
-% K = 40;
-% lbmd = 0.04;
-% mu= 0.005;
-% Q = 0.5;% this is wq without negative
+K = 60;
+lbmd = 0.04;
+mu= 0.01;
+Q = 0.5;% this is wq without negative
 SNR = 2000;
 
-K = [20, 40, 60, 80, 100, 120 ];
-lbmd = [0.005, 0.01,0.04, 0.07, 0.1 0.4, 0.7, 1 ];
-mu = [1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 5e-4, 1e-4];
-Q = [1, 0.9, 0.75, 0.5, 0.3 ]; % prtion
+%%%%%%%%%%%%%%%%%% step-1 5 fold's cross-validation %%%%%%%%%%%%%%%%%
+% K = [20, 40, 60, 80, 100, 120 ];
+% lbmd = [0.005, 0.01,0.04, 0.07, 0.1 0.4, 0.7, 1 ];
+% mu = [1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 5e-4, 1e-4];
+% Q = [1, 0.9, 0.75, 0.5, 0.3 ]; % prtion
+% result is cv = 0.8124 test = 0.832
+% K = 60;
+% lbmd = 0.04;
+% mu= 0.01;
+% Q = 0.5;% this is wq without negative, 
+%%%%%%%%%%%%%%%%%% step-1 5 fold's cross-validation %%%%%%%%%%%%%%%%%
 
-
+%%%%%%%%%%%%%%%%%% step-2 fine tuning 5 fold's cross-validation %%%%%%%%%%%%%%%%%
+K = 60;
+lbmd = 0.025:0.005:0.055;
+mu = [0.08 0.1 0.12 0.15 0.18 0.2 0.22];
+Q = [0.6, 0.5, 0.4 ]; % prtion
+% result is 
+% K = 60;
+% lbmd = 0.04;
+% mu= 0.01;
+% Q = 0.5;% this is wq without negative
+%%%%%%%%%%%%%%%%%% step-2 5 fold's cross-validation %%%%%%%%%%%%%%%%%
 
 %% load data
 [Database] = load_ESC(mixture_n, SNR, pctrl);
@@ -83,9 +100,9 @@ end
 end
 end
 end
-dt = datestr(datetime);
-dt((datestr(dt) == ':')) = '_'; % for windows computer
-save(['.././tempresult/m3log',dt, 't1_results'], 'acc_knn', 'acc_svm', 'maxknn', 'maxsvm', 'seed')
+% dt = datestr(datetime);
+% dt((datestr(dt) == ':')) = '_'; % for windows computer
+% save(['.././tempresult/m3log',dt, 't1_results'], 'acc_knn', 'acc_svm', 'maxknn', 'maxsvm', 'seed')
 end
 meanknn = max(max(max(max(sum(acc_knn,5)/5))));
 meansvm = max(max(max(max(sum(acc_svm,5)/5))));

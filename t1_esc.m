59680e49f59c821a20f0ec96c4f78649c9f32884
chenhao1UMBC <chenhao1@umbc.edu>
2018-12-28 19:04:10 -0800

    save current results

--- t1_esc.m
+++ t1_esc.m
@@ -7,7 +7,7 @@ tic
 
 addpath(genpath('.././fddlow'))
 addpath(genpath('.././data'))
-addpath(genpath('.././tempresult'))
+% addpath(genpath('.././tempresult'))
 
 %% load settings
 % do traing or do crossvalidation

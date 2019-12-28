911178e86616997fac836d1c9d53e16950e4ad95
chenhao1umbc <chenhao1@umbc.edu>
2019-12-27 22:08:49 -0500

    change file names

--- tt100.m
+++ tt100.m
@@ -5,16 +5,17 @@ clear
 clc;
 tic
 
-addpath(genpath('.././fddlow'))
-addpath(genpath('.././data'))
-addpath(genpath('.././FDDLPOW'))
+addpath(genpath('./core'));
+addpath(genpath('./etc'));
+addpath(genpath('/home/chenhao1/Matlab/FDDLOW/data'));
+SNR_INF = 2000;
+
 % do traing or do crossvalidation
 do_training = 1;
 do_cv = 0;
-
-% load data
 mixture_n = 1; % mixture_n classes mixture, = 1,2,3
 pctrl.db = 0; % dynamic ratio is 0 3, 6, 10, 20 db
+pctrl.if2weak = 0; % if 2 weak components in mixture of 3
 if pctrl.db == 0     pctrl.equal = 1; else    pctrl.equal = 0; end
 cvortest = 1;  % 1 means cv, 0 means test
 
@@ -22,31 +23,33 @@ cvortest = 1;  % 1 means cv, 0 means test
 % load settings
 K = 100;
 lbmd = 0.01;
-mu=1e-3;
+mu=0.001;
 nu= 1e3;
 beta = 1;
-Q=32;% this is wq without negative
-SNR = 2000;
+Q= 30;
+SNR = 20;
 
-% K = [50, 100, 150, 200];
-% lbmd = [0.1, 0.01, 0.001, 0.0001];
-mu = [0.1, 0.001];
+% K = [50, 100, 150, 200, 250];
+lbmd = [0.1, 0.01, 0.001, 1e-4];
+mu = [1, 0.1, 0.01, 0.001 0.0001];
 % SNR = [2000, 20, 0, -5, -10, -20];
-% Q = [10, 20, 30, 50, 75, 100];
+Q = [10 20 30 40 50 ];
 
-[Database]=load_data_new(mixture_n, SNR, pctrl);
+for f = 1000:1004
+[Database]=load_data_new(mixture_n, SNR_INF, pctrl, f);
 
-for ind1 = 1: length(K)
-for ind2 = 1: length(lbmd)
-for ind3= 1: length(mu)
-for Q = [50, 75, 100]
-    [opts]=loadoptions(K(ind1),lbmd(ind2),mu(ind3),Q,nu,beta, SNR)
+for indk = 1: length(K)
+for indq = 1: length(Q)
+for indl = 1: length(lbmd)
+for indm = 1: length(mu)   
     % for table 1 algorithm
     if do_training ==1
+        [opts] = loadoptions(K(indk),lbmd(indl),mu(indm),Q(indq),nu,beta,SNR,f)
         Dict = FDDLOW_table1(Database.tr_data,Database.tr_label,opts);
         save(opts.Dictnm,'Dict','opts')
     end
 end 
+end
 end
 end
 end

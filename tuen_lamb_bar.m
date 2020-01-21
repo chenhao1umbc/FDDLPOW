5805ace4dcecaa0373cc79fa8f25f5490ee31b9a
chenhao1umbc <chenhao1@umbc.edu>
2020-01-21 11:50:50 -0500

    adding the code of tuning lamb_bar

--- tuen_lamb_bar.m
+++ tuen_lamb_bar.m
@@ -27,9 +27,9 @@ for ind_snr = 1:length(SNR)
 %     db.test_data = awgn(db.test_data, SNR(ind_snr), 'measured');
 db = load_data_new(mixture_n, SNR(ind_snr), pctrl, 1000);   
 for f = 1000:1004
-for alg = 2:3
+for alg = 1:3
     
-%     if alg == 1 load(['dict1_k25_lmbd0.01_mu0.1_Q10_rng',num2str(f),'.mat']);disp(opts.Dictnm); end
+    if alg == 1 load(['dict1_k25_lmbd0.01_mu0.1_Q10_rng',num2str(f),'.mat']);disp(opts.Dictnm); end
     if alg == 2 
         load(['dict2_k25_lmbd0.1_mu0.001_Q20_nu10_rng',num2str(f),'.mat']);
         opts.lambda1 = 0.2;
@@ -66,132 +66,132 @@ plot(mean(r_knn,3)', '-x')
 
 
 
-% %% 
-% mixture_n = 2; 
-% 
-% r_zf = zeros(3,5,5); %3 algs; L=2; 5 dynamic ratio; 5 folds
-% r_mf = r_zf; r_zf_weak = r_zf; r_mf_weak = r_mf;
-% r_lr = r_zf; r_lr_weak = r_zf; r_nn_weak = r_zf; r_nn = r_zf;
-% dynamic_ratio = [0, 3, 6, 10, 20];    
-% for indd = 1:5
-%     pctrl.db = dynamic_ratio(indd); % dynamic ratio is 0 3, 6, 10, 20 db
-%     pctrl.if2weak = 0; % if 2 weak components in mixture of 3
-%     if pctrl.db == 0     pctrl.equal = 1; else    pctrl.equal = 0; end
-%     Database = load_data_new(mixture_n, SNR_INF, pctrl, 1000);   
-% for f = 1000:1004
-% for alg = 1:3
-% 
-%     if alg == 1 
-%         load(['dict1_k25_lmbd0.01_mu0.1_Q10_rng',num2str(f),'.mat']);
-%         load('B_X_Y_dict1.mat')
-%         load('NN_dict1.mat')
-%         disp(opts.Dictnm); 
-%     end
-%     if alg == 2 
-%         load(['dict2_k25_lmbd0.1_mu0.001_Q20_nu10_rng',num2str(f),'.mat']);
-%         load('B_X_Y_dict2.mat')
-%         load('NN_dict2.mat')
-%         opts.lambda1 = 0.05;
-%         disp(opts.Dict2nm); 
-%     end
-%     if alg == 3 
-%         load(['dict3_k25_lmbd0.1_mu0.001_Q20_nu10_beta1_rng',num2str(f),'.mat']);
-%         load('B_X_Y_dict3.mat')
-%         load('NN_dict3.mat')
-%         opts.lambda1 = 0.02;
-%         disp(opts.Dict3nm); 
-%     end
-%     
-%     run calc_M
-%     Z = sparsecoding(Dict, Database, opts, mixture_n, 0);
-%     Z = aoos(Z,Database.featln, size(Z, 2));
-%     % zero forcing
-%     H = W'*M;
-%     r_zeroforcing = pinv(H)*W'*Z;
-%     [~, labels_pre] = sort(r_zeroforcing, 1, 'descend');
-% 
-%     % matched filter
-%     r_matched = H'*W'*Z;
-%     [~, labels_pre_mf] = sort(r_matched, 1, 'descend');
-% 
-%     %neural networks
-%     outputs = net(W'*Z);
-%     [~, labels_pre_nn] = sort(outputs, 1, 'descend');
-%     
-%     % calculate accuracy
-%     [~, r_zf_weak(alg, indd, f-999), r_zf(alg, indd, f-999)] = calc_labels(labels_pre, opts);
-%     [~, r_mf_weak(alg, indd, f-999), r_mf(alg, indd, f-999)] = calc_labels(labels_pre_mf, opts);
-%     [~, r_nn_weak(alg, indd, f-999), r_nn(alg, indd, f-999)] = calc_labels(labels_pre_nn, opts);
-%     [~, r_lr_weak(alg, indd, f-999), r_lr(alg, indd, f-999)] = lr_test(Dict, Database, Z, B, pctrl);
-% 
-% end
-% end   
-% end
-% save('test_L=2.mat','r_zf', 'r_zf_weak', 'r_mf', 'r_mf_weak', 'r_nn','r_nn_weak','r_lr','r_lr_weak');
-% 
-% 
-% %% 
-% mixture_n = 3; 
-% 
-% r_zf = zeros(3,5,5); %3 algs; L=2; 5 dynamic ratio; 5 folds
-% r_mf = r_zf; r_zf_weak = r_zf; r_mf_weak = r_mf;
-% r_lr = r_zf; r_lr_weak = r_zf; r_nn_weak = r_zf; r_nn = r_zf;
-% dynamic_ratio = [0, 3, 6, 10, 20];    
-% for indd = 1:5
-%     pctrl.db = dynamic_ratio(indd); % dynamic ratio is 0 3, 6, 10, 20 db
-%     pctrl.if2weak = 0; % if 2 weak components in mixture of 3
-%     if pctrl.db == 0     pctrl.equal = 1; else    pctrl.equal = 0; end
-%     Database = load_data_new(mixture_n, SNR_INF, pctrl, 1000);   
-% for f = 1000:1004
-% for alg = 1:3
-% 
-%     if alg == 1 
-%         load(['dict1_k25_lmbd0.01_mu0.1_Q10_rng',num2str(f),'.mat']);
-%         load('B_X_Y_dict1.mat')
-%         load('NN_dict1.mat')
-%         disp(opts.Dictnm); 
-%     end
-%     if alg == 2 
-%         load(['dict2_k25_lmbd0.1_mu0.001_Q20_nu10_rng',num2str(f),'.mat']);
-%         load('B_X_Y_dict2.mat')
-%         load('NN_dict2.mat')
-%         opts.lambda1 = 0.05;
-%         disp(opts.Dict2nm); 
-%     end
-%     if alg == 3 
-%         load(['dict3_k25_lmbd0.1_mu0.001_Q20_nu10_beta1_rng',num2str(f),'.mat']);
-%         load('B_X_Y_dict3.mat')
-%         load('NN_dict3.mat')
-%         opts.lambda1 = 0.02;
-%         disp(opts.Dict3nm); 
-%     end
-%     
-%     run calc_M
-%     Z = sparsecoding(Dict, Database, opts, mixture_n, 0);
-%     Z = aoos(Z,Database.featln, size(Z, 2));
-%     % zero forcing
-%     H = W'*M;
-%     r_zeroforcing = pinv(H)*W'*Z;
-%     [~, labels_pre] = sort(r_zeroforcing, 1, 'descend');
-% 
-%     % matched filter
-%     r_matched = H'*W'*Z;
-%     [~, labels_pre_mf] = sort(r_matched, 1, 'descend');
-% 
-%     %neural networks
-%     outputs = net(W'*Z);
-%     [~, labels_pre_nn] = sort(outputs, 1, 'descend');
-%     
-%     % calculate accuracy
-%     [~, r_zf_weak(alg, indd, f-999), r_zf(alg, indd, f-999)] = calc_labels(labels_pre, opts);
-%     [~, r_mf_weak(alg, indd, f-999), r_mf(alg, indd, f-999)] = calc_labels(labels_pre_mf, opts);
-%     [~, r_nn_weak(alg, indd, f-999), r_nn(alg, indd, f-999)] = calc_labels(labels_pre_nn, opts);
-%     [~, r_lr_weak(alg, indd, f-999), r_lr(alg, indd, f-999)] = lr_test(Dict, Database, Z, B, pctrl);
-% 
-% end
-% end   
-% end
-% save('test_L=3.mat','r_zf', 'r_zf_weak', 'r_mf', 'r_mf_weak', 'r_nn','r_nn_weak','r_lr','r_lr_weak');
+%% 
+mixture_n = 2; 
+
+r_zf = zeros(3,5,5); %3 algs; L=2; 5 dynamic ratio; 5 folds
+r_mf = r_zf; r_zf_weak = r_zf; r_mf_weak = r_mf;
+r_lr = r_zf; r_lr_weak = r_zf; r_nn_weak = r_zf; r_nn = r_zf;
+dynamic_ratio = [0, 3, 6, 10, 20];    
+for indd = 1:5
+    pctrl.db = dynamic_ratio(indd); % dynamic ratio is 0 3, 6, 10, 20 db
+    pctrl.if2weak = 0; % if 2 weak components in mixture of 3
+    if pctrl.db == 0     pctrl.equal = 1; else    pctrl.equal = 0; end
+    Database = load_data_new(mixture_n, SNR_INF, pctrl, 1000);   
+for f = 1000:1004
+for alg = 1:3
+
+    if alg == 1 
+        load(['dict1_k25_lmbd0.01_mu0.1_Q10_rng',num2str(f),'.mat']);
+        load('B_X_Y_dict1.mat')
+        load('NN_dict1.mat')
+        disp(opts.Dictnm); 
+    end
+    if alg == 2 
+        load(['dict2_k25_lmbd0.1_mu0.001_Q20_nu10_rng',num2str(f),'.mat']);
+        load('B_X_Y_dict2.mat')
+        load('NN_dict2.mat')
+        opts.lambda1 = 0.05;
+        disp(opts.Dict2nm); 
+    end
+    if alg == 3 
+        load(['dict3_k25_lmbd0.1_mu0.001_Q20_nu10_beta1_rng',num2str(f),'.mat']);
+        load('B_X_Y_dict3.mat')
+        load('NN_dict3.mat')
+        opts.lambda1 = 0.02;
+        disp(opts.Dict3nm); 
+    end
+    
+    run calc_M
+    Z = sparsecoding(Dict, Database, opts, mixture_n, 0);
+    Z = aoos(Z,Database.featln, size(Z, 2));
+    % zero forcing
+    H = W'*M;
+    r_zeroforcing = pinv(H)*W'*Z;
+    [~, labels_pre] = sort(r_zeroforcing, 1, 'descend');
+
+    % matched filter
+    r_matched = H'*W'*Z;
+    [~, labels_pre_mf] = sort(r_matched, 1, 'descend');
+
+    %neural networks
+    outputs = net(W'*Z);
+    [~, labels_pre_nn] = sort(outputs, 1, 'descend');
+    
+    % calculate accuracy
+    [~, r_zf_weak(alg, indd, f-999), r_zf(alg, indd, f-999)] = calc_labels(labels_pre, opts);
+    [~, r_mf_weak(alg, indd, f-999), r_mf(alg, indd, f-999)] = calc_labels(labels_pre_mf, opts);
+    [~, r_nn_weak(alg, indd, f-999), r_nn(alg, indd, f-999)] = calc_labels(labels_pre_nn, opts);
+    [~, r_lr_weak(alg, indd, f-999), r_lr(alg, indd, f-999)] = lr_test(Dict, Database, Z, B, pctrl);
+
+end
+end   
+end
+save('test_L=2.mat','r_zf', 'r_zf_weak', 'r_mf', 'r_mf_weak', 'r_nn','r_nn_weak','r_lr','r_lr_weak');
+
+
+%% 
+mixture_n = 3; 
+
+r_zf = zeros(3,5,5); %3 algs; L=2; 5 dynamic ratio; 5 folds
+r_mf = r_zf; r_zf_weak = r_zf; r_mf_weak = r_mf;
+r_lr = r_zf; r_lr_weak = r_zf; r_nn_weak = r_zf; r_nn = r_zf;
+dynamic_ratio = [0, 3, 6, 10, 20];    
+for indd = 1:5
+    pctrl.db = dynamic_ratio(indd); % dynamic ratio is 0 3, 6, 10, 20 db
+    pctrl.if2weak = 0; % if 2 weak components in mixture of 3
+    if pctrl.db == 0     pctrl.equal = 1; else    pctrl.equal = 0; end
+    Database = load_data_new(mixture_n, SNR_INF, pctrl, 1000);   
+for f = 1000:1004
+for alg = 1:3
+
+    if alg == 1 
+        load(['dict1_k25_lmbd0.01_mu0.1_Q10_rng',num2str(f),'.mat']);
+        load('B_X_Y_dict1.mat')
+        load('NN_dict1.mat')
+        disp(opts.Dictnm); 
+    end
+    if alg == 2 
+        load(['dict2_k25_lmbd0.1_mu0.001_Q20_nu10_rng',num2str(f),'.mat']);
+        load('B_X_Y_dict2.mat')
+        load('NN_dict2.mat')
+        opts.lambda1 = 0.05;
+        disp(opts.Dict2nm); 
+    end
+    if alg == 3 
+        load(['dict3_k25_lmbd0.1_mu0.001_Q20_nu10_beta1_rng',num2str(f),'.mat']);
+        load('B_X_Y_dict3.mat')
+        load('NN_dict3.mat')
+        opts.lambda1 = 0.02;
+        disp(opts.Dict3nm); 
+    end
+    
+    run calc_M
+    Z = sparsecoding(Dict, Database, opts, mixture_n, 0);
+    Z = aoos(Z,Database.featln, size(Z, 2));
+    % zero forcing
+    H = W'*M;
+    r_zeroforcing = pinv(H)*W'*Z;
+    [~, labels_pre] = sort(r_zeroforcing, 1, 'descend');
+
+    % matched filter
+    r_matched = H'*W'*Z;
+    [~, labels_pre_mf] = sort(r_matched, 1, 'descend');
+
+    %neural networks
+    outputs = net(W'*Z);
+    [~, labels_pre_nn] = sort(outputs, 1, 'descend');
+    
+    % calculate accuracy
+    [~, r_zf_weak(alg, indd, f-999), r_zf(alg, indd, f-999)] = calc_labels(labels_pre, opts);
+    [~, r_mf_weak(alg, indd, f-999), r_mf(alg, indd, f-999)] = calc_labels(labels_pre_mf, opts);
+    [~, r_nn_weak(alg, indd, f-999), r_nn(alg, indd, f-999)] = calc_labels(labels_pre_nn, opts);
+    [~, r_lr_weak(alg, indd, f-999), r_lr(alg, indd, f-999)] = lr_test(Dict, Database, Z, B, pctrl);
+
+end
+end   
+end
+save('test_L=3.mat','r_zf', 'r_zf_weak', 'r_mf', 'r_mf_weak', 'r_nn','r_nn_weak','r_lr','r_lr_weak');
 
 
 

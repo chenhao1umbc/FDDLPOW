88686c92bc17af0a2f88c30e26a9d3bff93a942a
chenhao1 <chenhao1@umbc.edu>
2020-06-20 15:22:35 -0400

    removed some shared function, to make it run  without gui

--- lrsdl.m
+++ lrsdl.m
@@ -4,9 +4,9 @@ clear
 clc;
 tic
 
-addpath(genpath('.././fddlow'))
-addpath(genpath('.././data'))
-addpath(genpath('/extra/chenhao1/DICTOL-master/LRSDL_FDDL'))
+addpath(genpath('/extra/chenhao1/FDDLOW/fddlow'))
+addpath(genpath('/extra/chenhao1/FDDLOW/data'))
+addpath(genpath('/extra/chenhao1/DICTOL-master'))
 
 %% load data
 mixture_n = 2; % mixture_n classes mixture, = 1,2,3
@@ -19,15 +19,20 @@ else
     pctrl.equal = 0;
 end
 
-for f = 1000:1004
+% for f = 1000:1004
+f = 1000;
 [Database]=load_data_new(mixture_n, SNR_INF, pctrl, f);
 
 %% settings
-k = 5;
+for k = [3,5,7]
+    for lambda1 = [0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
+        for lambda2 = [0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
+            for lambda3 = [0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001]
+
 k0 = 3;
-lambda1 = 0.01;
-lambda2 = 0.01;
-lambda3 = 0.02;
+% lambda1 = 0.1;
+% lambda2 = 0.01;
+% lambda3 = 0.02;
 C = 6;
 
 opts.k           = k;
@@ -41,7 +46,7 @@ opts.D_range_ext = [opts.D_range k*C+k0];
 opts.initmode    = 'other';   
 opts.max_iter    = 100;
 opts             = initOpts(opts);
-opts.verbose      = true;
+opts.verbose      = false;
 opts.tol         = 1e-8;
 
 %% Train 
@@ -49,7 +54,7 @@ Y_train = Database.tr_data;
 Y_cv = Database.cv_data;
 label_train = Database.tr_label;
 label_cv = Database.cv_label;
-rmpath(genpath('.././fddlow'))  % to avoid the two fista.m confusion
+rmpath(genpath('/extra/chenhao1/FDDLOW/fddlow'))  % to avoid the two fista.m confusion
 [D, D0, X, X0, CoefM, CoefM0, opts, rt] = LRSDL(Y_train, label_train, opts);
 X1 = [X; X0];
 Y_range = label_to_range(label_train);
@@ -62,9 +67,14 @@ end
 
 param = ['k_',num2str(k),'k0_',num2str(k0), 'l1_',num2str(lambda1), ...
     'l2_',num2str(lambda2), 'l3_',num2str(lambda3), 'f_', num2str(f)];
-save([param,'lrscdl_train.mat'], 'D','D0','X', 'X0', 'CoefM','CoefM0', 'opts')
+% save([param,'lrscdl_train.mat'], 'D','D0','X', 'X0', 'CoefM','CoefM0', 'opts')
+
+printf(param)
 acc1 = LRSDL_pred(Y_cv, D, D0, CoefM, CoefM0, opts, label_cv);
-addpath(genpath('.././fddlow'))
+addpath(genpath('/extra/chenhao1/FDDLOW/fddlow'))
+end
+        end
+    end
 end
 
 
@@ -82,69 +92,4 @@ end
 
 
 
-
-function acc = LRSDL_pred(Y, D, D0, CoefM, m0, opts, label_test)
-nClasses = size(CoefM, 2);
-k = opts.k;
-k0 = opts.k0;
-D_range = k*(0:nClasses);
-
-N = size(Y,2);
-acc = [];
-% --------------- Sparse coding -------------------------
-for lambda1 = [0.001]
-    [X, X0] = local_sparse_coding(Y, D, D0, m0, opts.lambda1, opts.lambda2);
-    % --------------- classification -------------------------
-    Yhat = Y - D0*X0;
-    E1 = zeros(nClasses, N);
-    E2 = E1;
-    for c = 1: nClasses            
-        Dc = get_block_col(D, c, D_range);
-        Xc = get_block_row(X, c, D_range);
-        Mc = repmat(CoefM(:, c), 1, N );
-        R1 = Yhat - Dc*Xc;
-        R2 = X - Mc;
-        E1(c,:) = sum(R1.^2);
-        E2(c,:) = sum(R2.^2);
-    end
-    for w = [ .5]
-        E = w*E1 + (1-w)*E2;
-        [~, pred] = min(E);
-        aaaa = double(sum(pred == label_test))/N;
-        acc = [acc aaaa];
-        fprintf('w: %f, lambda1 = %.4f,  acc: %f\n', w, lambda1, aaaa);
-    end 
-end 
-
-    function [X, X0] = local_sparse_coding(Y, D, D0, m0, lambda1, lambda2)
-        N      = size(Y,2);
-        k      = size(D,2);
-        k0     = size(D0,2);
-        X1init = zeros(k + k0, N);
-        D1     = [D D0];
-        M0     = repmat(m0, 1, N);
-        D1tD1  = D1'*D1;
-        D1tY   = D1'*Y;
-        %% cost
-        function cost = calc_F(X1)
-            X = X1(1: k, :);
-            X0 = X1(k+1:end,:);
-            cost =  0.5*normF2(Y - D1*X1) + ...
-                    0.5*lambda2*normF2(X0 - M0) + ...
-                    lambda1*norm1(X1);
-        end 
-        %% grad
-        function g = grad(X1)
-            X  = X1(1: k, :);
-            X0 = X1(k+1:end,:);
-            g  = (D1tD1*X1 - D1tY + lambda2* [zeros(k, N); X0 - M0]);
-        end     
-        %% ========= Main FISTA ==============================
-        L             = max(eig(D1tD1)) + 2;
-        opts.tol      = 1e-8;
-        opts.max_iter = 300;
-        [X1, ~]       = fista(@grad, X1init, L, lambda1, opts, @calc_F);  
-        X             = X1(1: k, :);
-        X0            = X1(k+1:end,:);
-    end 
-end 
+ 
